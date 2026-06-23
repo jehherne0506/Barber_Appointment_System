ae7c393eb5c58e8f0d1888593ee21b6a2e48108e
jehherne <yeohjehherne@gmail.com>
2026-06-23 16:01:31 +0800

    Fix route issue

--- server/server.js
+++ server/server.js
@@ -1405,12 +1405,12 @@ function processVoucherPrice(voucherFound, originalPrice){
 }
 
 // 1. Serve Static Assets (The React App Build)
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // 2. Catch-All Route (For React Router)
 // If a user goes to /profile or /login directly, serve index.html
 app.get(/^\/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 async function startServer() {
